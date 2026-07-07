d30e02d0ed708dfbf4c07acbface177f75da223c
ipkf-prime <hamzeh001@gmail.com>
2026-07-07 17:56:47 +0330

    fix: harden database config defaults

--- public_html/config/database.php
+++ public_html/config/database.php
@@ -5,11 +5,11 @@ return [
     'connections' => [
 
         'mysql' => [
-            'host' => $_ENV['DB_HOST'],
-            'database' => $_ENV['DB_NAME'],
-            'username' => $_ENV['DB_USER'],
-            'password' => $_ENV['DB_PASS'],
-            'port' => $_ENV['DB_PORT'],
+            'host' => $_ENV['DB_HOST'] ?? 'localhost',
+            'database' => $_ENV['DB_NAME'] ?? '',
+            'username' => $_ENV['DB_USER'] ?? 'root',
+            'password' => $_ENV['DB_PASS'] ?? '',
+            'port' => $_ENV['DB_PORT'] ?? 3306,
         ]
 
     ]
